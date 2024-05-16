0bcbc0dcceaab40f8805d8c53da254607d097870
Meet-Prajapati <159993984+Meet-Prajapati@users.noreply.github.com>
2024-05-16 17:31:23 +0530

    third commit

--- 03_conversionoperation.js
+++ 03_conversionoperation.js
@@ -53,11 +53,26 @@ let str2="Meet";
 
 //console.log(str1+str2);
 
-console.log("1"  + 2);
-console.log(1+ "2");
-console.log("1"+ "2");
-console.log("1"+2+2);
-console.log(1+2+"2");
+// console.log("1"  + 2);
+// console.log(1+ "2");
+// console.log("1"+ "2");
+// console.log("1"+2+2);
+// console.log(1+2+"2");
 
-console.log((3*2)+(5%3));
+//console.log((3*2)+(5%3));
+
+//console.log(+true);
+//console.log(+"");
+
+let num1,num2,num3
+
+num1=num2=num3=2+2
+
+console.log(num3)
+
+let a=10;
+const b=a++
+console.log("a:"+a+"b:"+b);
+
+
 

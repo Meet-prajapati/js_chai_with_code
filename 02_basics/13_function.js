ea04149010c93eb29ae7c83e3edee4cb192f34ec
Meet-Prajapati <159993984+Meet-Prajapati@users.noreply.github.com>
2024-05-24 21:02:42 +0530

    last commit

---
+++ 02_basics/13_function.js
@@ -0,0 +1,21 @@
+function sayMyname() {
+  console.log("M");
+  console.log("E");
+  console.log("E");
+  console.log("t");
+}
+
+// sayMyname()
+
+function addTwoNUmbers(Number1,Number2){//parameters
+        // console.log(Number1+Number2)
+        let result=Number1+Number2
+        console.log("Meet");
+        return result
+        
+}
+
+const result=addTwoNUmbers(12,3);//arguments
+
+console.log("Result :", result)
+

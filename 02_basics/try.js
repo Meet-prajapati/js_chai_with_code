81b7dc12db9d2df2583d3917ce9f0e1f081d18b8
Meet-Prajapati <159993984+Meet-Prajapati@users.noreply.github.com>
2024-05-26 15:26:55 +0530

    last commit

---
+++ 02_basics/try.js
@@ -0,0 +1,44 @@
+// // function myFunc(number1,numbe2){
+// //     return number1+numbe2
+// // }
+
+// // // console.log(myFunc(12,34))
+// // const h=myFunc(43,45)
+// // console.log(h);
+
+// // function mySecondFun(number1,number2)
+// // {
+// //     let result=number1+number2
+// //     return result
+// // }
+// // console.log(mySecondFun(23,34))
+
+// // function calculater(...num1){
+// //     return num1
+// // }
+
+// // console.log(calculater(19,34,45,5));
+
+// const meet={
+//     username:"vaibhav",
+//     id:34
+// }
+// function forObjects(myObjects){
+//     console.log(`Userid is ${myObjects.username} an UserName is : ${myObjects.id}`)
+// }
+
+// forObjects(meet)
+// forObjects({
+//     username :"vrundaji",
+//     id:23
+// })
+const myArray=[23,34,3,43]
+function returnArrayValues(getArray){
+    console.log(getArray[2])
+}
+
+returnArrayValues(myArray)
+returnArrayValues([23,666,777,888,555,434])
+
+
+
